22a0272ce91bc2788215cb761458553d07888cdd
Developer_07 <72653396+Mauritz1709@users.noreply.github.com>
2021-05-02 11:01:34 +0200

    PATCH-3.7

--- Code/Bank.py
+++ Code/Bank.py
@@ -17,8 +17,14 @@ with open(fr"C:\Users\{os.getlogin()}\Documents\GitHub\J.A.N.I.N.E-KI\Code\Data.
 #Language for ERROR
 if data['Profile']['Language'] == "DE":
     Lang = "DE"
+    data['Profile']['Currency-Symbol'] = "\u20ac"
+    with open(fr"C:\Users\{os.getlogin()}\Documents\GitHub\J.A.N.I.N.E-KI\Code\Data.json", "w+") as f:
+        json.dump(data, f, indent=4)
 elif data['Profile']['Language'] == "EN":
     Lang = "EN"
+    data['Profile']['Currency-Symbol'] = "$"
+    with open(fr"C:\Users\{os.getlogin()}\Documents\GitHub\J.A.N.I.N.E-KI\Code\Data.json", "w+") as f:
+        json.dump(data, f, indent=4)
 
 #ERROR
 global ERROR_NO_PERMISSIONS
@@ -83,4 +89,31 @@ def Reset_Account(account: str):
     with open(fr"C:\Users\{os.getlogin()}\Documents\GitHub\J.A.N.I.N.E-KI\Code\Data.json", "w+") as f:
         json.dump(data, f, indent=4)
 
+#Deposit
+def Deposit(amount: int):
+    M = Enough_Money(amount, "Wallet")
+    if M == True:
+        data['Profile']['Wallet'] -= amount
+        data['Profile']['Bank'] += amount
+        with open(fr"C:\Users\{os.getlogin()}\Documents\GitHub\J.A.N.I.N.E-KI\Code\Data.json", "w+") as f:
+            json.dump(data, f, indent=4)
+        return True
 
+#Withdraw
+def Withdraw(amount):
+    M = Enough_Money(amount, "Bank")
+    if M == True:
+        data['Profile']['Bank'] -= amount
+        data['Profile']['Wallet'] += amount
+        with open(fr"C:\Users\{os.getlogin()}\Documents\GitHub\J.A.N.I.N.E-KI\Code\Data.json", "w+") as f:
+            json.dump(data, f, indent=4)
+        return True
+
+#Balance
+def Balance():
+    Bank = data['Profile']['Bank']
+    Wallet = data['Profile']['Wallet']
+    Currency = data['Profile']['Currency-Symbol']
+    print(f" Bank: {Bank}{Currency} \n\n Wallet: {Wallet}{Currency}")
+
+Balance()
